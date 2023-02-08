85e46421701b5b86c9efe21696d5bd20eb143d9d
FrogDesk Strategy <3432147+frogdesk@users.noreply.github.com>
2023-02-07 23:40:17 -0700

    Add SCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import type { UserConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+const config: UserConfig = {
+	plugins: [sveltekit()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: '@use "src/variables.scss" as *;'
+			}
+		}
+	}
+};
+
+export default config;
